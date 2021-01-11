7784d949ba4a9fae8b16277ffd9bea6d75b24d97
Primoris Robotics <30162213+opylypo@users.noreply.github.com>
2021-01-11 01:53:37 -0500

    Create insertion.py

---
+++ insertionsort_python/insertion.py
@@ -0,0 +1,22 @@
+  
+# Complete the insertion sort function
+
+# List of numbers to sort
+numbers = [1, 8, 4, 6, 0, 3, 5, 2, 7, 9]
+
+
+def main():
+    insertion_sort(numbers)
+
+    for number in numbers:
+        print(number, end=" ")
+    print()
+
+
+# Sort list using insertion sort
+def insertion_sort(numbers):
+    # TODO
+    return
+
+
+main()
